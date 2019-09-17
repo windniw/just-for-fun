63b3840103e949aad740f6e414850c6087703708
windniw <windniw36@gmail.com>
2019-09-17 23:07:39 +0800

    code(leetcode): fix 50

--- leetcode/50.py
+++ leetcode/50.py
@@ -7,6 +7,9 @@ problem: 快速幂
 solution: 用 x**n = (1/x)**(-n) 处理下 n 为负数情况即可
 另，其实直接return  x**n 也可以的，性能基本一致
 
+solution-fix: x**n = x**(2**i1 + 2**i2) = x**2**i1 * x**2**i2
+所以只要知道 x**2**i(i∈[1,logn]) 的值，当n的二进制的i位是1时，把值往上乘即可，省了递归步骤 
+
 """
 
 class Solution:
@@ -19,3 +22,16 @@ class Solution:
     def myPow(self, x: float, n: int) -> float:
         if n == 0: return 1
         return self.__pow(x, n) if n > 0 else self.__pow(1 / x, -n)
+
+# ---
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n < 0:
+            x = 1 / x
+            n = -n
+        res, t = 1, x
+        while n != 0:
+            if n & 1: res *= t
+            t *= t
+            n >>= 1
+        return res
