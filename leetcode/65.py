c85835c08afac1be352323d505f1a038fe386e0f
windniw <windniw36@gmail.com>
2019-10-01 14:11:36 +0800

    code(leetcode): fix 65

--- leetcode/65.py
+++ leetcode/65.py
@@ -6,6 +6,8 @@ problem: 问字符串是否为合法数字
 
 solution: 只要正则写得好，没有墙角挖不倒
 
+solution-fix: 合并多个正则，但实际跑起来没有明显时间差异
+
 """
 
 class Solution:
@@ -20,3 +22,9 @@ class Solution:
             if re.match(rr, s) is not None:
                 return True
         return False
+
+# ---
+class Solution:
+    def isNumber(self, s: str) -> bool:
+        r = '^\s*[+-]?((\d+(\.\d*)?)|(\d*\.\d+))(e[+-]?\d+)?\s*$'
+        return re.match(r, s) is not None
