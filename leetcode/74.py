1c754d73e035b23e3b3f050ad571c39444eafae4
windniw <windniw36@gmail.com>
2019-10-11 23:39:02 +0800

    code(leetcode): fix 74

--- leetcode/74.py
+++ leetcode/74.py
@@ -6,6 +6,8 @@ problem: 升序二维数组，问target是否存在
 
 solution: 两次二分，先行后列
 
+solution-fix: 压缩二维成一维，做matrix[x] --> matrix[x//m][x%m]映射即可
+
 """
 
 class Solution:
@@ -34,3 +36,21 @@ class Solution:
             else:
                 l = mid + 1
         return matrix[row][l] == target
+
+# --- 
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if len(matrix) == 0 or len(matrix[0]) == 0:
+            return False
+
+        n, m = len(matrix), len(matrix[0])
+        l, r = 0, n * m - 1
+
+        while l < r:
+            mid = (l + r) >> 1
+            if target <= matrix[mid // m][mid % m]:
+                r = mid
+            else:
+                l = mid + 1
+
+        return matrix[l // m][l % m] == target
