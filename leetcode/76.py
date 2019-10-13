119b9d8f5347b3e48bfd5a31b8e3c1bce671ff6d
windniw <windniw36@gmail.com>
2019-10-14 00:59:03 +0800

    code(leetcode): fix 76

--- leetcode/76.py
+++ leetcode/76.py
@@ -6,6 +6,8 @@ problem: 求s的子串s'，满足条件s'中包含t的所有字符
 
 solution: 双指针扫，当右指针第一次找到子串时，左指针开始向前直至不满足子串要求，然后继续交替遍历
 
+solution-fix: 优化检查项，去掉每次检查match的遍历，少了10ms
+
 """
 
 class Solution:
@@ -45,3 +47,24 @@ class Solution:
                     res_r = r
             r += 1
         return "" if res == len(s) + 1 else s[res_l:res_r + 1]
+
+# --- 
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(s) == 0 or len(t) == 0: return ""
+        m = collections.defaultdict(int)
+        for c in t:
+            m[c] += 1
+
+        l, match, res_l, res_r = 0, 0, 0, len(s)
+        for r, right_char in enumerate(s):
+            m[right_char] -= 1
+            if m[right_char] >= 0:
+                match += 1
+            if match == len(t):
+                while m[s[l]] < 0:
+                    m[s[l]] += 1
+                    l += 1
+                if r - l < res_r - res_l:
+                    res_l, res_r = l, r
+        return "" if match != len(t) else s[res_l:res_r + 1]
