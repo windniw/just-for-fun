202cd022dbf6f5aad68e708f3f752eefbc6ad5b6
windniw <windniw36@gmail.com>
2019-10-25 22:49:29 +0800

    code(leetcode): fix 88

--- leetcode/88.py
+++ leetcode/88.py
@@ -6,6 +6,8 @@ problem: 合并两个有序数组
 
 solution: 归并排序
 
+solution: 题目特殊性，可以原地排序，因为剩余空间在nums1的后半段，所以倒序归并，直接使用nums1的空间可以省去O(n)的空间开销
+
 """
 
 class Solution:
@@ -33,3 +35,21 @@ class Solution:
             k += 1
         for i in range(len(nums)):
             nums1[i] = nums[i]
+
+# --- 
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        i, j, k = m - 1, n - 1, n + m - 1
+        while i >= 0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+        if j >= 0:
+            nums1[:j + 1] = nums2[:j + 1]
