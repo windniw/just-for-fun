ec386257e4a5b4ac3115e7bd7d31d36ad0a92c19
windniw <windniw36@gmail.com>
2019-11-04 02:13:44 +0800

    code(leetcode): add 100.py

---
+++ leetcode/100.py
@@ -0,0 +1,24 @@
+"""
+
+link: https://leetcode.com/problems/same-tree
+
+problem: 判断两棵树是否完全一致
+
+solution: 递归检查
+
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
