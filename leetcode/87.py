88d4075093bf84c58318ce7cfb2940f6b0164af6
windniw <windniw36@gmail.com>
2019-11-11 00:04:22 +0800

    code(leetcode): fix 87

--- leetcode/87.py
+++ leetcode/87.py
@@ -11,6 +11,10 @@ s2[p2:p2+k]。
 递推式为 f[k][p1][p2] = f[i][p1][p2] && f[k-i][p1+i][p2+i]    // 不翻转子串情况
                     || f[i][p1][p2+k-i] && f[k-i][p1+i][p2]  // 翻转子串情况
 其中 i ∈ [1,k)
+
+solution-fix: 强行递归遍历，不得不说python的高阶函数真是好用，做双重优化
+- 上cache做备忘录
+- 搜索时如果两个字符串字符不一致就剪枝
 """
 
 class Solution:
@@ -31,3 +35,20 @@ class Solution:
                         f[k][p1][p2] |= (f[i][p1][p2] and f[k - i][p1 + i][p2 + i]) or (
                                 f[i][p1][p2 + k - i] and f[k - i][p1 + i][p2])
         return f[l][0][0]
+
+# --- 
+class Solution:
+    @functools.lru_cache(maxsize=None)
+    def isScramble(self, s1: str, s2: str) -> bool:
+        if len(s1) != len(s2):
+            return False
+        l = len(s1)
+        if l == 0 or l == 1 and s1[0] == s2[0]:
+            return True
+        if collections.Counter(s1) != collections.Counter(s2):
+            return False
+        for i in range(1, l):
+            if self.isScramble(s1[:i], s2[:i]) and self.isScramble(s1[i:], s2[i:]) or \
+                    self.isScramble(s1[:i], s2[l - i:]) and self.isScramble(s1[i:], s2[:l - i]):
+                return True
+        return False
