4f21d78967b5ca8df7ec83d1d23f06d577a54b37
windniw <windniw36@gmail.com>
2019-11-18 23:18:42 +0800

    code(leetcode): add 116

---
+++ leetcode/116.py
@@ -0,0 +1,29 @@
+"""
+
+link: https://leetcode.com/problems/populating-next-right-pointers-in-each-node
+
+problem: 对完美二叉树的每个节点补充 next 值，定义为在其右侧的节点，要求用常数空间或递归
+
+solution: 递归。对每个子树，其根节点只需关联左右儿子，及通过next，关联右儿子与兄弟的左儿子。
+
+"""
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left, right, next):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return root
+        if root.left:
+            root.left.next = root.right
+            if root.next:
+                root.right.next = root.next.left
+        self.connect(root.left)
+        self.connect(root.right)
+        return root
