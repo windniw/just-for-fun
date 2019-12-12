075bb0564d333e2789e20787a90fbcdebae99e3d
windniw <windniw36@gmail.com>
2019-12-13 00:21:25 +0800

    code(leetcode): fix 138

--- leetcode/138.py
+++ leetcode/138.py
@@ -6,6 +6,12 @@ problem: 拷贝链表，每个节点多一个random指针
 
 solution: 用字典存复制过的节点，先扫一遍复制正常链表，再扫一遍复制random指针
 
+solution-fix: 空间压缩到O(1)，不使用字典。
+              记原链表为 A -> B -> C ;
+              复制自己到下一位，修改为 A -> A' -> B -> B' -> C -> C' ;
+              扫一遍补上 random ;
+              分离原链表
+
 """
 
 """
@@ -32,4 +38,24 @@ class Solution:
                 pb.random = s[pa.random]
             pa = pa.next
             pb = pb.next
+        return fh.next
+# ---
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        t = head
+        while t:
+            t.next = Node(t.val, t.next, None)
+            t = t.next.next
+        t = head
+        while t:
+            if t.random:
+                t.next.random = t.random.next
+            t = t.next.next
+        fh = Node(0, None, None)
+        t, ft = head, fh
+        while t:
+            ft.next = t.next
+            ft = ft.next
+            t.next = t.next.next
+            t = t.next
         return fh.next
