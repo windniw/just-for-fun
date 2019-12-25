40c310f91c429a712de040ebb4067c4d818bd9fd
windniw <windniw36@gmail.com>
2019-12-25 23:05:33 +0800

    code(leetcode): add 135

---
+++ leetcode/135.py
@@ -0,0 +1,32 @@
+"""
+
+link: https://leetcode.com/problems/candy
+
+problem: 向数组项分配值，最小从1开始，当某项的权值大于其相邻项时，要求其分配的值也应大于该项, 求最小的分配和
+
+solution: 按权值排序，由低向高分配，当某项为中间的低谷时，分配1，否则大于其相邻的已分配项。时间O(nlogn)，空间O(n)
+
+"""
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        class child:
+            def __init__(self, score, idx):
+                self.score = score
+                self.idx = idx
+
+        s, candy = [child(ratings[i], i) for i in range(len(ratings))], [_ for _ in range(len(ratings))]
+        s.sort(key=lambda x: x.score)
+        for x in s:
+            left_score, left_candy, right_score, right_candy = float("inf"), 0, float("inf"), 0
+            if x.idx != 0:
+                left_score, left_candy = ratings[x.idx - 1], candy[x.idx - 1]
+            if x.idx != len(ratings) - 1:
+                right_score, right_candy = ratings[x.idx + 1], candy[x.idx + 1]
+
+            if x.score <= left_score and x.score <= right_score:
+                candy[x.idx] = 1
+            elif x.score > left_score and x.score > right_score:
+                candy[x.idx] = max(left_candy, right_candy) + 1
+            else:
+                candy[x.idx] = left_candy + 1 if x.score > left_score else right_candy + 1
+        return sum(candy)
