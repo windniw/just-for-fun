122d49dd01765557f7c22995197cc78cd4be4785
windniw <windniw36@gmail.com>
2020-01-20 23:37:54 +0800

    code(leetcode): add 201

--- leetcode/201.py
+++ leetcode/201.py
@@ -4,7 +4,10 @@ link: https://leetcode.com/problems/bitwise-and-of-numbers-range
 
 problem: 求 [m,n] 区间的与
 
-solution: 按位检查，若该位进1是否还在区间内
+solution: 自低位向高位看m，按位检查，若该位进1是否还在区间内
+
+solution-fix: 自高位向低位看n，找最高位使m,n不同的1，显然再往下的每一位要么经历过进位，要么均为0，则其与
+              肯定为 0; 问题转变为如何找到最高的不同的位，由低向高移除n的每个最低的1位即可
 
 """
 class Solution:
@@ -19,3 +22,10 @@ class Solution:
                 res += 1 << k
             k += 1
         return res
+
+"---
+class Solution:
+    def rangeBitwiseAnd(self, m: int, n: int) -> int:
+        while m < n:
+            n &= n - 1
+        return n
