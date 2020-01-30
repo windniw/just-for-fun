d493c40968ea4b7481d36538233ae2f2e16c72a8
windniw <windniw36@gmail.com>
2020-01-30 23:36:21 +0800

    code(leetcode): add 226

---
+++ leetcode/226.py
@@ -0,0 +1,24 @@
+"""
+
+link: https://leetcode.com/problems/invert-binary-tree
+
+problem: 翻转二叉树
+
+solution: 递归翻转子树
+
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return None
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        root.left, root.right = root.right, root.left
+        return root
