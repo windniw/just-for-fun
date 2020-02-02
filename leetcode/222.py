7ce3c968e59f831d7a505a7892ebc60b13b2a922
windniw <windniw36@gmail.com>
2020-02-02 23:39:24 +0800

    code(leetcode): add 222

---
+++ leetcode/222.py
@@ -0,0 +1,21 @@
+"""
+
+link: https://leetcode.com/problems/count-complete-tree-nodes
+
+problem: 计算完全二叉树结点数量
+
+solution: DFS
+
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def countNodes(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
