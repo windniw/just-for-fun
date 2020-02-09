6cad1faa23a3ba1d7555bccac0bcf40cf2c59f9c
windniw <windniw36@gmail.com>
2020-02-09 23:32:07 +0800

    code(leetcode): fix 230

--- leetcode/230.py
+++ leetcode/230.py
@@ -6,6 +6,8 @@ problem: 查找二叉排序树第k小的元素
 
 solution: 转数组返回对应下标
 
+solution-fix: 用栈循环代替递归，找到第k时直接返回，避免遍历整棵树
+
 """
 # Definition for a binary tree node.
 # class TreeNode:
@@ -27,3 +29,16 @@ class Solution:
 
         dfs(root)
         return l[k-1]
+# ---
+class Solution:
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        t, s = root, []
+        while True:
+            while t:
+                s.append(t)
+                t = t.left
+            t = s.pop()
+            k -= 1
+            if k == 0:
+                return t.val
+            t = t.right
