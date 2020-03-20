54db974462d3f52b344ff6d08a18a0dedbf9deba
windniw <windniw36@gmail.com>
2020-03-20 23:22:54 +0800

    code(leetcode): add 344

---
+++ leetcode/344.py
@@ -0,0 +1,17 @@
+"""
+
+link: https://leetcode.com/problems/reverse-string
+
+problem: 原地翻转字符串
+
+solution: 遍历交换
+
+"""
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        n = len(s)
+        for i in range(n // 2):
+            s[i], s[n - i - 1] = s[n - i - 1], s[i]
