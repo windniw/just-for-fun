3ec759f3b7e6cecef7d9be127b586ceb565a6693
windniw <windniw36@gmail.com>
2020-03-28 23:08:24 +0800

    code(leetcode): fix 322

--- leetcode/322.py
+++ leetcode/322.py
@@ -6,6 +6,8 @@ problem: n种无限数量的硬币，问能组成总额amount的最小数量
 
 solution: DP。dp[k] 为总额k的最少数量，dp[k+x] = min(dp[k] + x) x∈coins
 
+solution-fix: 除了自下而上做DP，也可以自上而下做备忘录。
+
 """
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
@@ -20,3 +22,19 @@ class Solution:
                     break
                 dp[x + k] = min(dp[x + k], dp[x] + 1)
         return dp[amount] if dp[amount] != float("inf") else -1
+
+# ---
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        @functools.lru_cache(maxsize=None)
+        def visit(x: int) -> int:
+            if not x:
+                return 0
+            res = float("inf")
+            for k in coins:
+                if x >= k:
+                    res = min(res, visit(x - k))
+            return res + 1
+
+        x = visit(amount)
+        return -1 if x == float("inf") else x
