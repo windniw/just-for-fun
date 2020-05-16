c87b5f793bee272f4b4fcf4dabb141e7616db1d0
windniw <windniw36@gmail.com>
2020-05-16 23:43:33 +0800

    code(leetcode): add 560

---
+++ leetcode/560.py
@@ -0,0 +1,18 @@
+"""
+
+link: https://leetcode.com/problems/subarray-sum-equals-k
+
+problem: 求数组中是否存在字串满足其和为给定数字
+
+solution: 记录前缀和。
+
+"""
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        s, cur, res = {0: 1}, 0, 0
+        for x in nums:
+            if cur + x - k in s:
+                res += s[cur + x - k]
+            cur += x
+            s[cur] = s[cur] + 1 if cur in s else 1
+        return res
