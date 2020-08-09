3afd98fb87d0278b534752a99c497ab5fc1a9af3
windniw <windniw36@gmail.com>
2020-08-09 23:43:45 +0800

    code(leetcode): fix 277

--- leetcode/277.py
+++ leetcode/277.py
@@ -6,6 +6,10 @@ problem: 求n个节点的图中入度为n-1，出度为0的点。
 
 solution: 暴力。枚举查每个节点。时间O(n^2)。
 
+solution-fix: 考虑入度为 n-1 ，出度为 0 的条件，即任意两个节点a,b间若 a -> b，则a肯定不是合法点，否则b肯定不是合法点。
+              令初始可能的合法点为0，扫一遍，当不满足 x -> 0 时，替换可能的合法点，则扫完一轮时必然只存在一个可能的节点。
+              检查该节点是否满足条件。时间O(n)
+
 """
 # The knows API is already defined for you.
 # return a bool, whether a knows b
@@ -31,3 +35,16 @@ class Solution:
             if famous:
                 return i
         return -1
+
+# ---
+class Solution:
+    def findCelebrity(self, n: int) -> int:
+        j = 0
+        for i in range(1, n):
+            if knows(j, i):
+                j = i
+        for i in range(n):
+            if i != j and (not knows(i, j) or knows(j, i)):
+                return -1
+        return j
+
