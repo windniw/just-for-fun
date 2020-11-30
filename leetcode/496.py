c9ae05cf7c407d9d728dd8b7097da24db5771676
windniw <windniw36@gmail.com>
2020-12-01 00:37:53 +0800

    code(leetcode): fix 496

--- leetcode/496.py
+++ leetcode/496.py
@@ -2,9 +2,11 @@
 
 link: https://leetcode-cn.com/problems/next-greater-element-i
 
-problem: 求 nums1 中元素 x，在 nums2 中的下一个大与其的值
+problem: 求 nums1 中元素 x，在 nums2 中的下一个大于其的值
 
 solution: 字典加搜索。
+
+solution-fix: 单调栈。先预查找 nums2 中每个元素的下一个大于其的值
 
 """
 class Solution:
@@ -19,4 +21,18 @@ class Solution:
                 elif nums2[j] > v:
                     res.append(nums2[j])
                     break
+        return res
+
+# ---
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        stack, m, res = [], {}, []
+        for k in nums2:
+            while stack and stack[-1] < k:
+                m[stack.pop()] = k
+            stack.append(k)
+        while stack:
+            m[stack.pop()] = -1
+        for k in nums1:
+            res.append(m[k])
         return res
